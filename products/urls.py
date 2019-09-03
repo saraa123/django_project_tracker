8a3ea5c71b2541a9bf639c340b71cafc335d9a10
saraa <address>
2019-09-03 18:03:08 +0100

    Changed all products url name

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from .views import all_products
 
 urlpatterns = [
     # Shows us the all_products view
-    url(r'^$', all_products, name='products')
+    url(r'^$', all_products, name='all_products')
 ]
