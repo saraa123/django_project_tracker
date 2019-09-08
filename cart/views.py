98a5acead0ec6d0ff5571e3bdb1f34da0a9de963
saraa <address>
2019-09-08 15:49:21 +0100

    Added id, and fixed solution when adding the same item to cart

--- cart/views.py
+++ cart/views.py
@@ -4,17 +4,22 @@ def view_cart(request):
     """ View cart items """
     return render(request, 'cart.html')
 
-def add_to_cart(request):
+def add_to_cart(request, id):
     """ Add items to cart """
     quantity = int(request.POST.get('quantity'))
 
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
-
+    
+    if id in cart:
+        cart[id] = int(cart.get(id) + quantity)
+    else:
+        cart[id] = cart.get(id, quantity)
+    
+    
     request.session['cart'] = cart
     return redirect(reverse('all_products'))
 
-def adjust_cart(request):
+def adjust_cart(request, id):
     """ Adjust the quantity of an item in the cart """
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
