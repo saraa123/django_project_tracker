34c99d59ef5eca024870f04bf5b93b7d3350b99f
saraa <address>
2019-09-10 21:38:41 +0100

    Deleted images in media url

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
 
 # All media stored here
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/images/'
+MEDIA_URL = '/media/'
 
 STRIPE_PUBLISHABLE = os.getenv("STRIPE_PUBLISHABLE")
 STRIPE_SECRET = os.getenv("STRIPE_SECRET")
