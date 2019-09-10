9e7b4d67116a4086042ce0fa2ce7f4769b368412
saraa <address>
2019-09-10 18:11:07 +0100

    Registered features to admin site and imported features from models

--- to_do/admin.py
+++ to_do/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Item 
-# Register your models here.
+from .models import Item, Feature
+
+# Register use of issues in admin 
 admin.site.register(Item)
+
+# Register use of features in admin
+admin.site.register(Feature)
