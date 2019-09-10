f321db162e7d5c6df9880fa6c7f65858aa6105e6
saraa <address>
2019-09-10 21:41:07 +0100

    Corrected spelling mistake in comment section

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product 
+from .models import Product
 
-# Enable admin panel to add procuts to site 
+# Enable admin panel to add products to site
 admin.site.register(Product)
