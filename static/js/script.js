1f3b11ada383a04e53bddc9ed88841b60276a974
saraa <address>
2019-10-01 10:11:40 +0100

    Initial commit

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $("button.login_to_like").click(function(){
+        alert("Login to like");
+    });
+
+    $("button.empty_quantity_alert").click(function() {
+        alert("Please add a quantity");
+    });
+});
+
