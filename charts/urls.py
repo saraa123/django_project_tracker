2855e58acdc55107dd4d7b7d320f614f906f7e4f
saraa <address>
2019-10-11 14:19:34 +0100

    Imported ChartData view and deleted pie_chart url pattern

--- charts/urls.py
+++ charts/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from charts.views import charts_home, pie_chart, ChartData
+from charts.views import charts_home, ChartData
 
 urlpatterns = [
     url(r'^charts$', charts_home, name="charts"),
-    url(r'^api-data/$', pie_chart, name="api-data"),
     url(r'^api-chart-data/$', ChartData.as_view()),
     
 ]
