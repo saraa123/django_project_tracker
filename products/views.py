454c25863f4133a22e911ec1636849d4044de61c
saraa <address>
2019-10-18 19:41:28 +0100

    Deleted comment that was pre-written by Django

--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Product
 
-# Create your views here.
 def all_products(request):
     # Return all products in the database
     products = Product.objects.all()
