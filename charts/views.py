b2f83d65cc5f0e74507b8f1e5fde2a66beb9d94c
saraa <address>
2019-10-18 19:30:24 +0100

    Imported count, case, and when from django models in order to count issues and features, and to also filter them by the ones that were completed and still pending, added extra data and labels for features which will be used in the charts html file

--- charts/views.py
+++ charts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import render, render_to_response
+from django.db.models import Count, Case, When
 
 from to_do.views import Item, Feature
 
@@ -18,15 +19,8 @@ users = get_user_model()
 
 # Load charts page
 def charts_home(request):
-    return render(request, "charts.html")
-
-
-def pie_chart(request):
-    data = {
-        "sales": 100,
-        "customers": 10,
-    }
-    return JsonResponse(data)
+    items = Item.objects.all()
+    return render(request, "charts.html", {"item": items})
 
 
 class ChartData(APIView):
@@ -35,13 +29,62 @@ class ChartData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
-        user_count = User.objects.all().count()
-        issue_count = Item.objects.all().count()
-        feature_count = Feature.objects.all().count()
-        labels = ["Users", "Issues", "Features", "green", "purple", "orange"]
-        default_items = [user_count, issue_count, feature_count, 5, 5, 5]
+    
+        
+        done_issues_count = []
+        done_features_count = []
+
+        to_do_issues_count = []
+        to_do_features_count = []
+
+        # chart labels and data for issues done and pending
+        labels = ["Issues completed", "Issues To Do"]
+        default_items = [done_issues_count, to_do_issues_count]
+
+        # Completed issues count
+        Issues_Done_Count = Item.objects.aggregate(
+            done_results=Count(Case(When(done=True, then='done')))
+        )
+
+        for k, v in Issues_Done_Count.items():
+            done_issues_count.append(v)
+
+        # Pending issues count 
+        Issues_Not_Done_Count = Item.objects.aggregate(
+            not_done_results=Count(Case(When(done=False, then='done')))
+        )
+
+        for k,v in Issues_Not_Done_Count.items():
+            to_do_issues_count.append(v)
+
+
+        # chart labels and data for features done and pending 
+        progressLabels = ["Features Completed", "Features To Do"]
+        progressItems = [done_features_count, to_do_features_count]
+
+
+        # Completed features count
+        Features_done_count = Feature.objects.aggregate(
+            done_features=Count(Case(When(done=True, then='done')))
+        )
+
+        for k, v in Features_done_count.items():
+            done_features_count.append(v)
+
+        # Count for features left to do 
+        features_not_done_count = Feature.objects.aggregate(
+            not_done_features=Count(Case(When(done=False, then='done')))
+        )
+
+        for k,v in features_not_done_count.items():
+            to_do_features_count.append(v)
+        
+        
         data = {
             "labels": labels,
             "default": default_items,
+            "progress_labels": progressLabels,
+            "progress_default": progressItems
         }
+
         return Response(data)
