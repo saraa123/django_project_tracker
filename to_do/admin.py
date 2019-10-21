00c6955eee090211515defd803deefc9ccbf0674
saraa <address>
2019-10-21 12:08:21 +0100

    Added list displays for 'timestamp' and 'done' and filter lists to admin model

--- to_do/admin.py
+++ to_do/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Item, Feature
 
-# Register use of issues in admin 
-admin.site.register(Item)
+class ItemModelAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "timestamp", "done"]
+    list_filter = ["done", "timestamp"]
+    class Meta:
+        model = Item
+
+
+# Register use of issues in admin
+admin.site.register(Item, ItemModelAdmin)
 
 # Register use of features in admin
 admin.site.register(Feature)
