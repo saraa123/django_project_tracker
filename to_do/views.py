d46f3d31401fe8cd5a0a302cb5bdcfd9fcfd312a
saraa <address>
2019-10-31 16:11:40 +0000

    Imported reverse, User, Count, Case and When from Django, added issue and feature counts to todo_list view, added doc strings to views where required, deleted edit_an_item view and updated the toggle_status view to check if the user is logged in, as well as adding the ability to toggle features.

--- to_do/views.py
+++ to_do/views.py
@@ -1,14 +1,56 @@
-from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404, reverse 
 from .models import Item, Feature
 from .forms import ItemForm, FeatureForm
+from django.contrib.auth.models import User
+from django.db.models import Count, Case, When
+
 
 # Create your views here.
 def todo_list(request):
+
+    # Issue objects 
     results = Item.objects.all()
+
+    # feature objects 
     feature_results = Feature.objects.all()
-    return render(request, 'todo_list.html', {"items": results, "name": feature_results})
+
+    # Count array for issues that are left to do 
+    not_done_issues_count = []
+
+    # Count array for features left to do 
+    pending_features_to_do_count = []
+
+    # Count for issues that are done 
+    Done_results = Item.objects.aggregate(
+        done_results=Count(Case(When(done=True, then='done')))
+    )
+
+
+# pending issues to do count
+    Issues_Not_Done_Count = Item.objects.aggregate(
+        not_done_results=Count(Case(When(done=False, then='done')))
+    )
+
+    for k, v in Issues_Not_Done_Count.items():
+            not_done_issues_count.append(v)
+
+
+# features pending to do count
+    features_not_done_count = Feature.objects.aggregate(
+        not_done_features=Count(Case(When(done=False, then='done')))
+    )
+
+    for k,v in features_not_done_count.items():
+            pending_features_to_do_count.append(v)
+
+    
+    return render(request, 'todo_list.html', 
+                  {"items": results, "name": feature_results, "Done_results": Done_results, 
+                  "not_done_issues_count": not_done_issues_count, "pending_features_to_do_count": pending_features_to_do_count})
 
 def new_item(request):
+    "Function for adding a new issue"
+
     if request.method=='POST':
         form = ItemForm(request.POST, request.FILES)
         if form.is_valid():
@@ -19,26 +61,33 @@ def new_item(request):
 
     return render(request, 'add_new_item.html', {'form': form})
 
-def edit_an_item(request, id):
-        item = get_object_or_404(Item, pk=id)
-        form = ItemForm(instance=item)
-
-        if request.method == 'POST':
-                form = ItemForm(request.POST, instance=item)
-
-                if form.is_valid():
-                        form.save()
-                        return redirect(todo_list)
-        else:
-                form = ItemForm(instance=item)
-
-
-        
-        return render(request, 'edit_issue.html', {'form': form})
 
 
 def toggle_status(request, id):
-        item = get_object_or_404(Item, pk=id)
-        item.done = not item.done
-        item.save()
-        return redirect(todo_list)
+    "Function to upvote an issue or feature"
+
+    user = request.user
+
+    if user.is_authenticated():
+            # if user likes an issue
+
+            if Item.objects.filter(pk=id).exists():
+                    item = get_object_or_404(Item, pk=id)
+                    if user in item.likes.all():
+                            item.likes.remove(user)
+                    else:
+                            item.likes.add(user)
+
+            # if user likes a feature 
+
+            if Feature.objects.filter(pk=id).exists():
+                    feature = get_object_or_404(Feature, pk=id)
+                    if user in feature.likes.all():
+                            feature.likes.remove(user)
+                    else:
+                            feature.likes.add(user)
+
+        
+    return redirect(todo_list)
+
+
