dd6c2a13d52c6d9696ad63cbf13d43b387e8c7ae
saraa <address>
2019-10-31 17:01:39 +0000

    Changed name from 'timestamp' to 'date_created' in the ItemModelAdmin class

--- to_do/admin.py
+++ to_do/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Item, Feature
 
 class ItemModelAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "timestamp", "done"]
-    list_filter = ["done", "timestamp"]
+    list_display = ["__str__", "done", "date_created"]
+    list_filter = ["done", "date_created"]
     class Meta:
         model = Item
 
