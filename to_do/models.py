a053ecfdc6de9258301c1d704346dd2af528886b
saraa <address>
2019-10-31 17:00:18 +0000

    Imported settings from django, changed 'name' from the Item model to Issue, added a Feature model, added last_updated, date_created and likes to both models

--- to_do/models.py
+++ to_do/models.py
@@ -1,9 +1,29 @@
 from django.db import models
+from django.conf import settings 
 
 # Create your models here.
 class Item(models.Model):
+    
+    Issue = models.CharField(max_length=30, blank=False)
+    done = models.BooleanField(blank=False, default=False)
+
+    last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+
+    def __str__(self):
+        return self.Issue
+    
+class Feature(models.Model):
     name = models.CharField(max_length=30, blank=False)
     done = models.BooleanField(blank=False, default=False)
+
+    last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+
 
     def __str__(self):
         return self.name
