25d5c24de731253fc157f65643bfc8f5a7521fcb
saraa <address>
2020-02-09 16:22:18 +0000

    Added docstrings, added and updated comments, and changed code layout.

--- to_do/views.py
+++ to_do/views.py
@@ -5,41 +5,43 @@ from django.contrib.auth.models import User
 from django.db.models import Count, Case, When
 
 
-# Create your views here.
 def todo_list(request):
+    """ Home page """ 
 
     # Issue objects 
     results = Item.objects.all()
 
-    # feature objects 
+    # Feature objects 
     feature_results = Feature.objects.all()
 
-    # Count array for issues that are left to do 
+    # List that will be used to count the issues that are left to do 
     not_done_issues_count = []
 
-    # Count array for features left to do 
+    # List that will be used to count the features that are left to do
     pending_features_to_do_count = []
 
-    # Count for issues that are done 
+    # Count how many issues are marked as 'done' 
     Done_results = Item.objects.aggregate(
         done_results=Count(Case(When(done=True, then='done')))
     )
 
 
-# pending issues to do count
+    # Count how many issues are still pending
     Issues_Not_Done_Count = Item.objects.aggregate(
         not_done_results=Count(Case(When(done=False, then='done')))
     )
 
+    # Append the number of issues not done to the not_done_issues_count list
     for k, v in Issues_Not_Done_Count.items():
             not_done_issues_count.append(v)
 
 
-# features pending to do count
+    # Features pending to do count
     features_not_done_count = Feature.objects.aggregate(
         not_done_features=Count(Case(When(done=False, then='done')))
     )
 
+    # Append number of features still pending to the pending_features_to_do_count list
     for k,v in features_not_done_count.items():
             pending_features_to_do_count.append(v)
 
@@ -48,8 +50,9 @@ def todo_list(request):
                   {"items": results, "name": feature_results, "Done_results": Done_results, 
                   "not_done_issues_count": not_done_issues_count, "pending_features_to_do_count": pending_features_to_do_count})
 
+
 def new_item(request):
-    # Function for adding a new issue
+    """ Function for adding a new issue """
 
     if request.method=='POST':
         form = ItemForm(request.POST, request.FILES)
@@ -64,13 +67,14 @@ def new_item(request):
 
 
 def toggle_status(request, id):
-    # Function to upvote an issue or feature
+    """ Function to upvote an issue or feature """ 
 
     user = request.user
 
+    # Check if the user is logged in
     if user.is_authenticated():
-            # if user likes an issue
 
+            # If user likes an issue
             if Item.objects.filter(pk=id).exists():
                     item = get_object_or_404(Item, pk=id)
                     if user in item.likes.all():
@@ -78,8 +82,7 @@ def toggle_status(request, id):
                     else:
                             item.likes.add(user)
 
-            # if user likes a feature 
-
+            # If user likes a feature 
             if Feature.objects.filter(pk=id).exists():
                     feature = get_object_or_404(Feature, pk=id)
                     if user in feature.likes.all():
