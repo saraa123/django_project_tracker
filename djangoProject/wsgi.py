6e034dd7767d897e57d974378e7ee3ad27720be8
saraa <address>
2020-03-11 12:51:30 +0000

    Added secret key, stripe publishable and stripe secret properties.

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject.settings")
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
+STRIPE_PUBLISHABLE = os.environ.get("STRIPE_PUBLISHABLE")
+STRIPE_SECRET = os.environ.get("STRIPE_SECRET")
+
 application = get_wsgi_application()
