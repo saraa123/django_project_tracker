8253ab4ecaac6e78b07f9b73164a8b00f38eb61c
saraa <address>
2020-03-17 18:29:47 +0000

    Added Feedback class.

--- to_do/models.py
+++ to_do/models.py
@@ -28,3 +28,10 @@ class Feature(models.Model):
     def __str__(self):
         return self.name
 
+class Feedback(models.Model):
+    feedback = models.CharField(max_length=200, blank=False)
+    date_added = models.DateTimeField(auto_now=False, auto_now_add=True)
+    name = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.feedback
