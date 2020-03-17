75efda15ed25c3de824fa75daa2f2f6eed03debf
saraa <address>
2020-03-17 18:19:04 +0000

    Deleted template docstring, and added url for adding feedback.

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,23 +1,9 @@
-"""djangoProject URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 
 # urls for to_do app
-from to_do.views import todo_list, new_item, toggle_status
+from to_do.views import todo_list, new_item, add_feedback, toggle_status
 
 # urls for accounts app
 # from accounts.views import index, logout, login, profile
@@ -47,7 +33,8 @@ from .settings import MEDIA_ROOT
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', todo_list, name='home'), # home refers to to_do view from to_do app
-    url(r'^add$', new_item),
+    url(r'^add$', new_item, name='new_issue'),
+    url(r'^add_feedback$', add_feedback, name='add_feedback'),
     url(r'^toggle/(?P<id>\d+)$', toggle_status),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(products_urls)),
