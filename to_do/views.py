621bb1aa3abeaf84936b4d84b9409bd5b6bb0cdb
saraa <address>
2020-03-26 12:41:27 +0000

    Changed feature_results to features when querying the database, this is also the name it has been changed to when passing features through to be returned, removed feedback from home page, added a for loop to get the amount it costs to like a feature and the amount received, added comments, added an instance of the feedback form for the add_feedback function as well as checking if the user is logged in, and the toggle function has logic to see how much it costs to like a feature and how much money this equals. It will also check if we have reached our target.

--- to_do/views.py
+++ to_do/views.py
@@ -15,7 +15,7 @@ def todo_list(request):
     results = Item.objects.all()
 
     # Feature objects 
-    feature_results = Feature.objects.all()
+    features = Feature.objects.all()
 
     # List that will be used to count the issues that are left to do 
     not_done_issues_count = []
@@ -48,18 +48,27 @@ def todo_list(request):
     for k,v in features_not_done_count.items():
             pending_features_to_do_count.append(v)
 
-    feedback = Feedback.objects.all()
 
-    random_feedback = random.choice(feedback)
+    for feature in features:
+        like_cost = feature.like_cost
+        amount_got = feature.likes.count() * like_cost
 
+        print("feature name", feature.name)
+        print("like cost", feature.like_cost)
+        print("like amount", feature.likes.count())
+        print("amount_got",amount_got)
+        print("--------------------")
+
+
+    
     return render(request, 'todo_list.html', 
                   {"items": results, 
-                  "name": feature_results, 
+                  "features": features, 
                   "Done_results": Done_results, 
                   "not_done_issues_count": not_done_issues_count, 
                   "pending_features_to_do_count": pending_features_to_do_count,
-                  "feedback": feedback,
-                  "random_feedback": random_feedback})
+                  "amount_got": amount_got
+                  })
 
 
 def new_item(request):
@@ -79,25 +88,39 @@ def new_item(request):
 def add_feedback(request):
     """ Function for adding feedback """
 
+    # Query database - find all feedback
     feedback = Feedback.objects.all()
 
+    # Randomly displays feedback
     random_feedback = random.choice(feedback)
 
+    # The feedback form
+    form = FeedbackForm(request.POST, request.FILES)
 
+    
     if request.method == 'POST':
         form = FeedbackForm(request.POST, request.FILES)
+
         if form.is_valid():
             messages.success(request, "Thanks for your feedback!")    
             form.save()
             return redirect(todo_list)
-    else:
+        else:
             form = FeedbackForm()
 
 
+     # Check if the user is logged in
+    if not request.user.is_authenticated:
+            form.name='anonymous'
+            user=form.name
+    else:
+        user = User.objects.get(email=request.user.email)
+        
     return render(request, 'add_feedback.html', 
                 {'form': form,
                 "feedback": feedback,
-                "random_feedback": random_feedback})
+                "random_feedback": random_feedback,
+                "user": user})
 
 
 
@@ -108,7 +131,7 @@ def toggle_status(request, id):
 
     # Check if the user is logged in
     if user.is_authenticated():
-
+            
             # If user likes an issue
             if Item.objects.filter(pk=id).exists():
                     item = get_object_or_404(Item, pk=id)
@@ -120,12 +143,23 @@ def toggle_status(request, id):
             # If user likes a feature 
             if Feature.objects.filter(pk=id).exists():
                     feature = get_object_or_404(Feature, pk=id)
+                    like_cost = feature.like_cost
+
                     if user in feature.likes.all():
                             feature.likes.remove(user)
+                            amount_got = like_cost * feature.likes.count()
+                            
                     else:
                             feature.likes.add(user)
-
-        
+                            amount_got = like_cost * feature.likes.count()
+                    
+                    # See if the amount we have towards a feature equals the amount we need
+                    if amount_got >= like_cost:
+                        if amount_got == like_cost:
+                            print("We've hit our target!")
+                        elif amount_got > like_cost:
+                            print("Over our target!")
+            
     return redirect(todo_list)
 
 
