79720d9b231faa4634b2b42e4c86f6e061d34684
saraa <address>
2020-04-03 13:49:37 +0100

    Added a function that pops up an alert and highlights the cart and product count when an item is added to the basket.

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,20 @@ $(document).ready(function() {
     });
 });
 
+
+
+// Called on products.html page
+// Adds a message to the user that the item has been added to their basket 
+// and also adds custom classes as an alert
+var confirm = document.getElementsByClassName("add_confirm");
+
+var alertAdd = function() {
+    document.getElementById("confirmAlertMessage").innerHTML = "Added to basket";
+    $(".productCount").addClass("confirm_alert")
+    $("#shopping_bag").addClass("shopping_bag_alert")
+
+};
+
+for (var i = 0; i < confirm.length; i++) {
+    confirm[i].addEventListener('click', alertAdd, false);
+}
