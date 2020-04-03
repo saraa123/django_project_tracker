182dcda2dc7e3d7673288c1898492245643c55b8
saraa <address>
2020-04-03 10:31:33 +0100

    Changed comment to docstring.

--- products/views.py
+++ products/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Product
 
 def all_products(request):
-    # Return all products in the database
+    """ Return all products in the database """
     products = Product.objects.all()
     return render(request, 'products.html', {"products": products})
