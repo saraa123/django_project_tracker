392324ae6c407f952e6ea97d6ceebae0acef8f3d
saraa <address>
2020-04-20 11:46:18 +0100

    Added urls for upvote_checkout and the invoice page.

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import checkout 
+from .views import checkout, upvote_checkout, invoice
 
 urlpatterns = [
     url(r'^$', checkout, name='checkout'),
+    url(r'^checkout/upvote_checkout/(?P<id>\d+)$', upvote_checkout, name='upvote_checkout'),
+    url(r'^invoice$', invoice, name='invoice')
 ]
