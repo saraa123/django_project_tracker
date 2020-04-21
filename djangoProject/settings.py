3c09b0c12c51ca360a7d99b4bd3b7c7bf4712c50
saraa <address>
2020-04-21 11:25:12 +0100

    Removed email_backend and the commented out code for email_host and email_port, replaced them with gmail smtp to send users emails.

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -150,7 +150,8 @@ STRIPE_PUBLISHABLE = os.environ.get("STRIPE_PUBLISHABLE")
 STRIPE_SECRET = os.environ.get("STRIPE_SECRET")
 
 """ Allow functionality to email user if they have forgotten their password """
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-# EMAIL_HOST = 'localhost'
-# EMAIL_PORT = 1025
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
