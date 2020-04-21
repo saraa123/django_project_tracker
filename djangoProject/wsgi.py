659294789200d4ac8c014ab3f10fb0b97bfb9be3
saraa <address>
2020-04-21 11:26:32 +0100

    Added EMAIL_HOST_USER and EMAIL_HOST_PASSWORD env variables to send emails.

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -16,5 +16,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject.settings")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 STRIPE_PUBLISHABLE = os.environ.get("STRIPE_PUBLISHABLE")
 STRIPE_SECRET = os.environ.get("STRIPE_SECRET")
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 application = get_wsgi_application()
