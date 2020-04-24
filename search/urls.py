d449ba77b5bf789c8fc6082f19eafbe11f313852
saraa <address>
2020-04-24 20:35:14 +0100

    Added url for search_issues_and_features.

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url 
-from .views import search_product
+from .views import search_product, search_issues_and_features
 
 urlpatterns = [
-    url(r'^$', search_product, name='search')
+    url(r'^$', search_product, name='search'),
+    url(r'^search_issues_features$', search_issues_and_features, name='search_issues_and_features')
 ]
