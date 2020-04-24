14133f037f151eefee9a4d373e080995f6789a71
saraa <address>
2020-04-24 17:18:24 +0100

    Added a function to search issues and features and made the required imports; Q, Feature and Item.

--- search/views.py
+++ search/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Q
 from products.models import Product
+from to_do.models import Item, Feature
 
 def search_product(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, 'products.html', {"products": products})
+
+def search_issues_and_features(request):
+    """ Function to search issues and features """
+    
+    search_query = (Q(Item.objects.filter(Issue__icontains=request.GET['q']).values('Issue')) | Q(Feature.objects.filter(name__icontains=request.GET['q']).values('name')))
+    
+    return render(request, 'search_results.html', {"search_results": search_query})
 
