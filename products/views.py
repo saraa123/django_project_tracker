54c0c64bba212da1ba269ab28a576791340595f7
saraa <address>
2020-04-25 12:57:48 +0100

    Removed 'products' instance.

--- products/views.py
+++ products/views.py
@@ -3,5 +3,4 @@ from .models import Product
 
 def all_products(request):
     """ Return all products in the database """
-    products = Product.objects.all()
-    return render(request, 'products.html', {"products": products})
+    return render(request, 'products.html')
