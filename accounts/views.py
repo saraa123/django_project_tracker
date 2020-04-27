3e1394aa752fa9da35a9870aae598370890dd1c7
saraa <address>
2020-04-27 20:43:34 +0100

    Imported UpdateProfileForm and UserProfile and created a function that allows a user to edit their profile which currently only includes favourite games, and the registration page now takes first name and last name.

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from accounts.forms import UserLoginForm, UserRegistrationForm
+from accounts.forms import UserLoginForm, UserRegistrationForm, UpdateProfileForm
 from to_do.models import Feature, Item
 from checkout.models import Order
+from accounts.models import UserProfile
 
 def index(request):
     """Return the index.html file, which is the registration landing page """
@@ -18,7 +19,10 @@ def index(request):
             registration_form.save()
 
             user = auth.authenticate(username=request.POST['username'],
-                                password=request.POST['password1'])
+                                password=request.POST['password1'],
+                                first_name=request.POST['first_name'],
+                                last_name=request.POST['last_name']
+                                )
 
             if user:
                 auth.login(user=user, request=request)
@@ -88,3 +92,21 @@ def profile(request):
         "user_orders": user_orders,
         "user_liked_feature": user_liked_feature,
         "user_liked_issues": user_liked_issues})
+
+def edit_profile(request):
+    """ Function to edit the user profile - only favourite games is available at 
+    the moment """
+    
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been updated.")
+            return redirect('profile')
+    else:
+        form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, 'edit_profile.html', {
+        "form": form, 
+        "messages": messages
+    })
