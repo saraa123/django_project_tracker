6f22d562ca5ccaaa06af5ea158ec0fa1c9e72dea
saraa <address>
2020-04-27 20:28:14 +0100

    Imported UserProfile model and registered its use in admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "user"]
+    model = UserProfile
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
