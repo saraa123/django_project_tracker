e3e567fef0d5f0eb1d2831fb3d28d9916f6caacc
saraa <address>
2020-05-12 09:53:19 +0100

    Changes to the Order model; removed extra fields, and altered ordering of remaining fields.

--- checkout/models.py
+++ checkout/models.py
@@ -6,13 +6,10 @@ User = get_user_model()
 
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=20, blank=False)
+    street_address1 = models.CharField(max_length=50, blank=False)
     country = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=20, blank=True)
-    town_or_city = models.CharField(max_length=50, blank=False)
-    street_address1 = models.CharField(max_length=50, blank=False)
-    street_address2 = models.CharField(max_length=50, blank=False)
-    county = models.CharField(max_length=40, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
     date = models.DateField()
 
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='orders', default=1)
