f1b1fef5901411191021153c7fc82b68684fcf6e
saraa <address>
2020-05-23 10:18:15 +0100

    Returned credit card details and cvv to required=False.

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,8 +6,8 @@ class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
     YEAR_CHOICES = [(i, i) for i in range(2020, 2040)]
 
-    credit_card_number = forms.CharField(label='Credit card number', required=True)
-    cvv = forms.CharField(label='Security code (CVV)', required=True)
+    credit_card_number = forms.CharField(label='Credit card number', required=False)
+    cvv = forms.CharField(label='Security code (CVV)', required=False)
     expiry_month = forms.ChoiceField(label='Exp Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Exp Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
